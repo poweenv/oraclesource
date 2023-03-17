db4ba6754b7b9f4ae6f518d0ddd19feddce0bb03
gwangyeol <kgkg12kg@naver.com>
2023-03-17 18:04:58 +0900

    scott 수정

--- scott.sql
+++ scott.sql
@@ -1,1 +1,85 @@
-#scott 
+--scott 
+
+--emp 테이블 구성 보기
+-- 필드명(열이름) 제약조건   데이터타입
+-- enpno(사원번호)not null   number(4)
+-- ename(사원명),job(직책),mgr(직속상관 번호),hiredate(입사일),sal(급여),comm(수당),deptno(부서번호) 
+-- number : 소수점 자릿수를 포함해서 지정가능
+-- number(4) : 4자리 숫자까지 허용 , number(8,2) : 전체 자릿수는 8자리이고 소수점 2자리를 포함한다.
+--varchar2 : 가변형 문자열 저장
+--varchar2 : 10byte 문자까지 저장 가능
+--date :  날쫘 데이터
+
+
+desc emp;
+--deptno(부서번호),dname(부서명),loc(부서위치)
+desc dept;
+--select: 데이터 조회
+desc salgrade
+--grade(급여등급),losal(최소 급여액),hisal(최대 급여액)
+
+--select : 데이터 조회
+--조회 방식: 섹렉션(행 단위로 조회),프로젝션(열 단위로 조회,조인(두개 이상의 테이블을 사용하여 조회)
+--select 열이름1, 열이름2.... (조회할 열이 전체라면*로 처리)
+--from 테이블명;
+
+--1.emp 테이블의 전체 열을 조회
+SELECT
+    *
+FROM
+    emp;
+
+--2. emp 테이블에서 사원번호,이름,급여 열을 조회
+--ctrl + f7 : 정렬
+SELECT
+    empno,
+    ename,
+    sal
+FROM
+    emp;
+
+SELECT
+    *
+FROM
+    emp;
+    
+--3. dept 테이블 전체 조회
+SELECT
+    *
+FROM
+    dept;
+--4. dept 테이블안에 부서번호, 지역만 조회
+SELECT
+    deptno,
+    loc
+FROM
+    dept;
+
+SELECT
+    deptno
+FROM
+    emp;
+
+--6. emp 테이블안에 부서번호 조회(단, 중복된 부서 번호는 제거)
+--distinct : 중복 제거
+SELECT DISTINCT
+    deptno
+FROM
+    emp;
+-- 열이 여러개인 경우 (묶어서 중복이냐 아니냐를 판단)
+SELECT DISTINCT
+    job,
+    deptno
+FROM
+    emp;
+    
+-- 7. 연산
+-- 별칭 : 필드에 별칭을 임의로 부여( as 별칭, 혹은 한칸 띄고 별칭, 별칭에 공백이 있다면 ""로 묶어주기
+-- ex)사원들의 1년 연봉 구하기 ( sal * 12 *comm)
+select empno,ename,sal * 12+ comm as ANNSAL from emp;
+select empno,ename 사원명,job 직책,sal * 12+ comm ANNSAL from emp;
+
+
+
+
+
--- system.sql
+++ system.sql
@@ -1,19 +1,19 @@
-# 대소문자 구별하지 않음(단 비밀번호는 구별함)
-# CREATE : 생성 / ALTER : 수정 / DROP : 삭제 / DELETE : 삭제
-
-#오라클 버전이 변경되면서 사용자 생성시 C## 문자를 넣어서 만들도록 변경 됨
-# JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
+-- 대소문자 구별하지 않음(단 비밀번호는 구별함)
+-- CREATE : 생성 / ALTER : 수정 / DROP : 삭제 / DELETE : 삭제
+--
+-- 오라클 버전이 변경되면서 사용자 생성시 C## 문자를 넣어서 만들도록 변경 됨
+-- JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
-#USER 생성 : 공간 할당
-#USER 생성은 SYS,SYSTEM 만 가능
-#CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
+--USER 생성 : 공간 할당
+--USER 생성은 SYS,SYSTEM 만 가능
+--CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
  CREATE USER JAVADB IDENTIFIED BY 1234
  DEFAULT TABLESPACE USERS
  TEMPORARY TABLESPACE TEMP;
 
 
-#GRANT : 권한 부여(사용자 생성만 해서는 아무것도 할 수 없음)
+--GRANT : 권한 부여(사용자 생성만 해서는 아무것도 할 수 없음)
 GRANT CONNECT, RESOURCE TO JAVADB;
 
  CREATE USER JAVADB IDENTIFIED BY TIGER
@@ -21,3 +21,11 @@ GRANT CONNECT, RESOURCE TO JAVADB;
  TEMPORARY TABLESPACE TEMP;
 
 GRANT CONNECT, RESOURCE TO SCOTT;
+
+select * from all_users;
+
+GRANT UNLIMITED TABLESPACE TO SCOTT;
+
+GRANT UNLIMITED TABLESPACE TO SCOTT;
+ALTER USER SCOTT DEFAULT TABLESPACE USERS;
+ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
