056336199b0c0cd4ccaaa203dd0c4ac0106f0467
poweenv <128015207+poweenv@users.noreply.github.com>
2023-03-18 20:15:35 +0900

    Update system.sql

--- system.sql
+++ system.sql
@@ -1,6 +1,6 @@
 -- 대소문자 구별하지 않음(단 비밀번호는 구별함)
 -- CREATE : 생성 / ALTER : 수정 / DROP : 삭제 / DELETE : 삭제
---
+------
 -- 오라클 버전이 변경되면서 사용자 생성시 C## 문자를 넣어서 만들도록 변경 됨
 -- JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
