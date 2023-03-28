a4b2ebb49e7cfa88f2f41f6d17fdddbf0970b347
gwangyeol <kgkg12kg@naver.com>
2023-03-28 18:05:27 +0900

    javadb update

--- javadb.sql
+++ javadb.sql
@@ -1,1 +1,84 @@
 --javadb
+
+-- userTBL 테이블 생성
+-- no(번호-숫자(4)), username(이름-한글(4)), birthYear(년도-숫자(4)),addr(주소-문자(한글,숫자)),mobile(010-1234-1234)
+-- no pk 제약조건 지정(제약조건명 pk_userTBL)
+create table userTBL(
+    no number(4) constraint pk_userTBL PRIMARY KEY,
+    username nvarchar2(10) not null,
+    birthYear number(4) not null,
+    addr varchar2(50) not null,
+    mobile varchar2(14)
+);
+
+drop table userTBL;
+
+
+-- 시퀀스 생성
+-- user_seq 생성(기본)
+create sequence user_seq;
+
+--insert
+-- no : user_seq 넣기
+insert into userTBL(no, username,birthyear,addr,mobile)
+ values(user_seq.NEXTVAL,'홍길동',2000,'종로구 5길 30','010-1111-2222');
+
+commit;
+
+-- commit == > git : 로컬저장소 저장, git bash,eclipse
+-- commit == > sql : DML 작업 시 한번에 실행되어야 하는 단위, DML 작업 한 후 commit(DB 최종 반영) 해야 함, sqldeveloper
+
+--  모든 컬럼 not null
+-- paytype : pay_no(number-1,pk), info(문자- card, cash)
+-- paytype_seq 시퀀스 생성
+create table paytype (
+    pay_no number(1) primary key,
+    info char(4) not null
+);
+alter table paytype modify info varchar2(10);
+create sequence paytype_seq;
+insert into paytype values(paytype_seq.NEXTVAL, 'card');
+insert into paytype values(paytype_seq.NEXTVAL, 'cash');
+select *from paytype;
+drop table paytype;
+
+-- shop
+-- user : user_id(숫자-4자리,pk),name(문자-한글), pay_no(숫자-1 : paytype table에 있는 pay_no 참조해서 사용)
+create table suser(
+    user_id number(4) primary key,
+    name nvarchar2(4) not null,
+    pay_no number(1) references paytype(pay_no) not null
+);
+alter table suser modify name varchar2(20);
+-- product
+-- product_id(숫자-8자리,pk), pname(문자), price(숫자), content(문자), 
+create table product(
+    product_id number(8) primary key,
+    pname nvarchar2(10) not null,
+    price number(10) not null,
+    content nvarchar2(20) not null
+);
+alter table product modify pname varchar2(30);
+alter table product modify content varchar2(50);
+create sequence product_seq;
+-- order
+-- order_id(숫자-8자리,pk), user_id(user 테이블의 user_id 참조), product_id(product 테이블의 product_id 참조)
+create table sorder(
+    order_id number(8) primary key,
+    user_id number(4) references suser(user_id) not null,
+    procuct_id number(8) references product(product_id) not null
+);
+create sequence order_seq;
+
+
+
+commit;
+
+
+
+
+
+
+
+
+
--- system.sql
+++ system.sql
@@ -5,13 +5,27 @@
 -- JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
+-- 사용자 : scott,hr => 데이터베이스 접속하여 관리하는 계정
+--        테이블, 인덱스, 뷰 등 여러 객체가 사용자별로 생성됨
+--        업무에 따라 사용자들을 나눠서 관리
+
+-- 스키마 : 데이터간 관계, 데이터 구조, 제약조건 등 데이터를 관리 저장하기 위해 정의한 데이터베이스 구조의 범위
+-- 스키마 == 사용자(오라클 데이터베이스 경우)
+
+-- 사용자 생성 : 사용자 생성 권한이 필요함 ==> sys, system
+
+
+
 --USER 생성 : 공간 할당
 --USER 생성은 SYS,SYSTEM 만 가능
 --CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
+-- 사용자 생성 
+-- 비밀번호는 대소문자 구별함 
  CREATE USER JAVADB IDENTIFIED BY 1234
  DEFAULT TABLESPACE USERS
  TEMPORARY TABLESPACE TEMP;
 
+alter user javadb quota 2m on users;
 
 --GRANT : 권한 부여(사용자 생성만 해서는 아무것도 할 수 없음)
 GRANT CONNECT, RESOURCE TO JAVADB;
@@ -34,3 +48,66 @@ GRANT UNLIMITED TABLESPACE TO SCOTT;
 GRANT UNLIMITED TABLESPACE TO SCOTT;
 ALTER USER SCOTT DEFAULT TABLESPACE USERS;
 ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
+
+-- test 사용자 생성 / 비번 1234
+CREATE USER test IDENTIFIED BY 1234;
+
+-- 접속 권한을 부여하지 않으면 의미가 없음
+
+-- 권한 권리
+-- 1) system 권한
+--    사용자 생성(create user)/ 수정(alter user)/ 삭제(drop user)
+--    데이터베이스 접근(create session) / 수정(alter session)
+--    여러 객체 생성(view,synonym) 및 관리 권한
+-- 2) 객체 권한
+--    테이블 수정, 삭제, 인덱스 생성, 삽입, 참조, 조회, 수정
+--    뷰 삭제, 삽입, 생성, 조회, 수정
+--    시퀀스 수정, 조회
+--    프로시저, 함수, 패키지 권한
+-- 권한 부여
+-- create session(데이터베이스 접근 권한) 을 test 에게 부여
+grant create session to test ;
+
+grant resource, create table to test;
+
+-- 테이블 스페이스 users에 권한이 없습니다
+ALTER USER test DEFAULT TABLESPACE USERS;
+ALTER USER test TEMPORARY TABLESPACE TEMP;
+alter user test quota 2m on users;   --users 에 공간 설정
+
+-- scott 에게 test 가 생성한 member 테이블 조회 권한 부여
+--grant select on member to scott;
+
+-- 권한 취소
+--REVOKE SELECT, INSERT ON member FROM scott;
+
+-- 롤 : 여러 권한들의 모임
+-- connect 롤 : create session
+-- resource 롤 : create triger, create sequence, create procedure, create table...
+
+
+-- 사용자 삭제
+drop user test;
+drop user test CASCADE;  --test와 관련된 모든 객체 같이 삭제
+
+-- 사용자 생성 + 테이블 스페이스 권한 부여
+ CREATE USER test2 IDENTIFIED BY 1234
+ DEFAULT TABLESPACE USERS
+ TEMPORARY TABLESPACE TEMP;
+-- 권한 부여
+grant connect,resource to test2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
